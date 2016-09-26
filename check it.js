9236978eeb03f523bba89e85784a73aa98589709
Nuborn2001 <nuborn2001@gmail.com>
2016-09-25 20:09:56 -0400

    Create check it.js

---
+++ check it.js
@@ -0,0 +1,87 @@
+
+
+
+sts_bs("",[20080623,"images/","","blank.gif",5,1,1,1,"100%","center",1,2,155,125,1,100,0,0,0,5000,1,40,2,"stEffect(\"scroll(Rate=24,enabled=0,Duration=0.50)\")",-2,60],["none",1,"#fb9124","transparent","","repeat"]);
+sts_til([0,"We Must Never Forget!!!!","center"],["bold 11pt Verdana,Arial","#B9090B","none","transparent","","repeat"]);
+sts_pag(["transparent","7pt Verdana,Arial","#B9090B","none","transparent","7pt Verdana,Arial","#B9090B","none","#B9090B","7pt Verdana,Arial","#FFFFFF","none","#FF6600","7pt Verdana,Arial","#FFFFFF","none"]);
+sts_sca(["center","middle","center","middle"],["https://dl.dropboxusercontent.com/u/12447336/tha%20nu/images/hot_fire.gif","https://dl.dropboxusercontent.com/u/12447336/tha%20nu/images/hot_fire.gif","arrowl_gray.gif",34,33,"https://dl.dropboxusercontent.com/u/12447336/tha%20nu/images/hot_fire.gif","https://dl.dropboxusercontent.com/u/12447336/tha%20nu/images/hot_fire.gif","arrowr_gray.gif",34,33]);
+
+
+
+sts_ai("i0",[0,"The Book-July","https://dl.dropboxusercontent.com/u/12447336/tha%20nu/tracebook july.html",target="top","https://dl.dropboxusercontent.com/u/12447336/tha%20nu/NuSheild_2.jpg",155,125,"center"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"]);
+sts_ai("i1",[,"Trayvon Martin:A Senseless Killing","https://www.youtube.com/watch?v=ETx5OLtXDZ0",target="top","https://i.ytimg.com/vi/ETx5OLtXDZ0/mqdefault.jpg"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i2",[,"Footage of Oscar Grant getting shot in the back by police","https://www.youtube.com/watch?v=5h-IEg8c6uI",target="top","https://i.ytimg.com/vi/46IQfVIAl8Y/mqdefault.jpg"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i3",[,"7 yr old Aiyana Jones killed in detroit by Police, Video of shooting, trial, funeral","https://www.youtube.com/watch?v=zjh_FevzUNQ",target="top","https://i.ytimg.com/vi_webp/zjh_FevzUNQ/mqdefault.webp"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i4",[,"NYPD Cops choke Eric Garner to death for selling a 50 cent Cigarette ! ... Extended Footage","https://www.youtube.com/watch?v=ajhpFT-lVZY",target="top","https://i.ytimg.com/vi_webp/RlYYhCQQFTQ/mqdefault.webp"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i5",[,"All 4 eyewitness accounts of the MURDER of Mike Brown in Ferguson, Missouri","https://www.youtube.com/watch?v=fRbiEZ_vIOQ",target="top","https://i.ytimg.com/vi_webp/vPmMxCiraQA/mqdefault.webp"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i6",[,"John Crawford Killed in Cold Blood? The Disturbing Walmart Shooting Surveillance Video Released","https://www.youtube.com/watch?v=0ltN_uW20nQ",target="top","https://i.ytimg.com/vi_webp/qNQK5MH5GRI/mqdefault.webp"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i7",[,"Cop Randall Kerrick shoots unarmed man Jonathan Ferrell 10 times in North Carolina","https://www.youtube.com/watch?v=UZQLcjdi02U",target="top","https://i.ytimg.com/vi/ugNymbjIQUY/mqdefault.jpg"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i8",[,"THE MURDER OF SEAN BELL","https://www.youtube.com/watch?v=dFEIlpm6HEM",target="top","https://i.ytimg.com/vi/4XhNQiFjbeA/mqdefault.jpg"],[],"i0","i0");
+sts_ai("i9",[,"The Amadou Diallo Shooting","https://www.youtube.com/watch?v=Imnen1TsCXI",target="top","https://i.ytimg.com/vi_webp/dj-_MfD-Ndg/mqdefault.webp"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+sts_ai("i10",[,"Police Murder Handcuffed Chavis Carter In Back Of Patrol Car","https://www.youtube.com/watch?v=mlQ0rMDFpeI",target="top","https://i.ytimg.com/vi/dQ8lTBRDiA0/mqdefault.jpg"],[],"i0","i0");
+sts_ai("i11",[,"RAW VIDEO: Tamir Rice(12 Years Old With a BB gun shot in 2 secs by police) police shooting surveillance video","https://www.youtube.com/watch?v=Xz_oPE_oUp4",target="top","https://i.ytimg.com/vi_webp/DQUJWZNJnSc/mqdefault.webp"],[],"i0","i0");
+
+
+sts_ai("i12",[,"Justice 4 Alan Blueford","https://www.youtube.com/watch?v=Mr25YhiFvN8",target="top","https://dl.dropboxusercontent.com/u/12447336/tha%20nu/alanblueford_opdvictim.png"],[],"i0","i0");
+
+
+
+sts_ai("i13",[,"No Charges for Cops who Taser Teen(Victor Steen after stopping him for a traffic violation he committed while riding his bike), Run Over â˜  Kill Him (MURDER)","https://www.youtube.com/watch?v=Mr25YhiFvN8",target="top","https://i.ytimg.com/vi/Z_WOpuKtAwo/mqdefault.jpg"],[],"i0","i0");
+
+
+sts_ai("i14",[,"Kendrick Johnson's body was found in a rolled up wrestling mat in the gymnasium of Lowndes High School, Valdosta, Georgia, USA,","https://www.youtube.com/watch?v=IyqDHsJqWwk",target="top","https://dl.dropboxusercontent.com/u/12447336/tha%20nu/Kendrick Johnson .jpg"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+
+
+
+sts_ai("i15",[,"The Murder of Fred Hampton","https://www.youtube.com/watch?v=gyMbFBebUE4",target="top","https://i.ytimg.com/vi/gyMbFBebUE4/mqdefault.jpg"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+
+
+
+sts_ai("i16",[,"Criminals Of Permission C.O.P ( #GrindFace )","https://www.youtube.com/watch?v=UWsr3bTP3NI",target="top","https://i.ytimg.com/vi_webp/UWsr3bTP3NI/mqdefault.webp"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+
+
+
+
+sts_ai("i17",[,"William Chapman killed by police officer who just came off suspension for killing.","https://www.youtube.com/watch?v=w1Ow4Elg-KE",target="top","https://i.ytimg.com/vi_webp/w1Ow4Elg-KE/mqdefault.webp"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+
+
+
+
+
+sts_ai("i18",[,"He even out his hands behind his back while they were punching him and he still got charged with resisting","https://www.youtube.com/watch?v=T-9NTBTDkvs",target="top","https://dl.dropboxusercontent.com/u/12447336/tha%20nu/pizza beating.gif"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+
+
+
+
+
+
+
+
+
+
+sts_ai("i19",[,"I Am Troy Davis State  SANCTIONED Murder Never Forget!!!!!!!","https://www.youtube.com/watch?v=BmYJvR6qaZA",target="top","https://i.ytimg.com/vi/jojW-ubwC5o/mqdefault.jpg"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+
+
+
+
+
+
+
+
+
+
+sts_ai("i19",[,"Save Our Kids (8/8/15): Christian Taylor, a 19-year old college football player was gunned down by police early Friday morning in Arlington, TX. Christian crashed his vehicle into a car dealership.  ","http://crimeblog.dallasnews.com/2015/08/arlington-police-officer-fatally-shot-a-man-early-friday-at-car-dealership.html/",target="top","http://41.media.tumblr.com/c71513b59237bb4ea553d616b08bd7bb/tumblr_nsqihf6NCO1qaf2nxo5_500.png"],["transparent","9pt Cooper Black","#B9090B","none","10pt Cooper Black","#B9090B","none"],"i0","i0");
+
+
+
+
+
+
+
+;
+sts_es();
+
+
+
+
+
